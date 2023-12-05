913f613e3d556db8ae920c2e599c9eb1c2f4a131
alanzarate <alnzarate@gmail.com>
2023-12-05 19:43:04 -0400

    first commit

---
+++ src/main/java/com/st/app/service/RobotManipulator.java
@@ -0,0 +1,18 @@
+package com.st.app.service;
+import java.awt.*;
+
+public class RobotManipulator {
+    private Robot bot;
+    public RobotManipulator(Robot... bot){
+        if(bot.length > 0 ){
+            if(bot)
+        }
+        if(bot != null){
+            this.bot = bot[0];
+        }
+    }
+    public void click(){
+
+    }
+}
+
